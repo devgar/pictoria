a9ee17e391b1a82ddd12465abece0b7d84fd7007
Edgar Albalate <dev.gardo@gmail.com>
2021-03-29 19:00:22 +0200

    Receives images and converts them to webp

--- main.go
+++ main.go
@@ -8,60 +8,52 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"syscall"
 
 	"github.com/chai2010/webp"
 )
 
-func DecodeWebP() {
-	var buf bytes.Buffer
-	var width, height int
-	var data []byte
-	var err error
-	if data, err = ioutil.ReadFile("image.webp"); err != nil {
-		log.Println(err)
+var storage string
+
+func initStorage() {
+	storage = os.Getenv("STORAGE")
+	if storage == "" {
+		if homedir, err := os.UserHomeDir(); err == nil {
+			storage = path.Join(homedir, ".pictoria")
+		} else {
+			log.Panic("Can't choose storage", err)
+		}
 	}
-	if width, height, _, err = webp.GetInfo(data); err != nil {
-		log.Println(err)
+	if err := os.MkdirAll(storage, os.ModeSetuid); err != nil {
+		log.Panic("Error creating storage", err)
 	}
 
-	fmt.Printf("width = %d, height = %d\n", width, height)
-
-	// GetMetadata
-	if metadata, err := webp.GetMetadata(data, "ICCP"); err != nil {
-		fmt.Printf("Metadata: err = %v\n", err)
-	} else {
-		fmt.Printf("Metadata: %s\n", string(metadata))
+	if err := syscall.Access(storage, syscall.O_RDWR); err != nil {
+		log.Panic("Can't create on desired storage ", err)
 	}
 
-	// Decode webp
-	m, err := webp.Decode(bytes.NewReader(data))
-	if err != nil {
-		log.Println(err)
-	}
+	log.Println("Storage in", storage)
+}
 
-	// Encode lossless webp
-	if err = webp.Encode(&buf, m, &webp.Options{Lossless: false}); err != nil {
-		log.Println(err)
-	}
-	if err = ioutil.WriteFile("output.webp", buf.Bytes(), 0666); err != nil {
-		log.Println(err)
-	}
-
-	fmt.Println("Save output.webp ok")
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	var route = path.Join(storage, r.URL.Path[1:])
+	log.Println("Using route", route)
+	http.ServeFile(w, r, route)
 }
 
 func main() {
 
-	// DecodeWebP()
-	// return
+	initStorage()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, r.URL.Path)
 		if r.Method == "GET" {
-			http.ServeFile(w, r, "image.jpg")
+			serveFile(w, r)
 		} else {
 			r.ParseMultipartForm(10 << 20) // Set max size to 10MB
-			if file, header, err := r.FormFile("file"); err != nil {
+			if file, _, err := r.FormFile("file"); err != nil {
 				fmt.Println("Error Retrieving the file")
 				fmt.Println(err)
 				w.WriteHeader(500)
@@ -69,9 +61,9 @@ func main() {
 				return
 			} else {
 				defer file.Close()
-				fmt.Printf("Uploaded File: %+v\n", header.Filename)
-				fmt.Printf("File Size:     %+v\n", header.Size)
-				fmt.Printf("MIME Header:   %+v\n", header.Header["Content-Type"])
+				// fmt.Printf("Uploaded File: %+v\n", header.Filename)
+				// fmt.Printf("File Size:     %+v\n", header.Size)
+				// fmt.Printf("MIME Header:   %+v\n", header.Header["Content-Type"])
 				// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 
 				img, text, err := image.Decode(file)
@@ -81,14 +73,19 @@ func main() {
 				log.Printf("Got string format %s", text)
 
 				var buf bytes.Buffer
+				var ops = &webp.Options{Lossless: false, Quality: 80}
 
-				if err := webp.Encode(&buf, img, &webp.Options{Lossless: false, Quality: 80}); err != nil {
+				if err := webp.Encode(&buf, img, ops); err != nil {
 					log.Println(err)
 				}
 
-				if err := ioutil.WriteFile("out.webp", buf.Bytes(), 0666); err != nil {
+				var dest = path.Join(storage, "out.webp")
+				if err := ioutil.WriteFile(dest, buf.Bytes(), 0666); err != nil {
 					log.Println(err)
 				}
+				var location = fmt.Sprintf("http://%s/%s", r.Host, dest)
+				w.Header().Add("location", location)
+				w.WriteHeader(201)
 				fmt.Fprintf(w, "Successfully Uploaded File\n")
 			}
 		}
